f7207239c796234ef81a617c6e152e71cb8c2b29
Adrien <a.bonnand@pacman.fr>
2023-09-12 12:02:49 +0200

    création de la fonction sur les couleurs partiellement correctes

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+'''Colors definition'''
+
+R="Red"
+G="Green"
+B="Blue"
+Y="Yellow"
+P="Purple"
+W="White"
+
+'''Ask the user to obtain a potential true combinaison'''
+def ask_combinaison():
+    combinaison = input("Write combinaison : first second third four")
+    combinaison = combinaison.split()
+    print(combinaison)
+    '''first_color = combinaison[0]
+    second_color = combinaison[1]
+    third_color = combinaison[2]
+    fourth_color = combinaison[3]
+    return(first_color+second_color+third_color+fourth_color)'''
+    return(combinaison)
+
+'''test if the user combinaison is the same that the true combinaison'''
+def partial_number_color(user_combinaison,true_combinaison):
+
+    partial_number=0;
+
+    for first_idx in range(len(user_combinaison)):
+        for second_idx in range(len(true_combinaison)):
+            if(user_combinaison[first_idx]==true_combinaison[second_idx] and user_combinaison[second_idx]!=true_combinaison[second_idx]):
+                partial_number+=1
+
+    return partial_number
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+user_combinaison=ask_combinaison()
+true_combinaison=['G','R','B','P']
+print(partial_number_color(user_combinaison,true_combinaison))
