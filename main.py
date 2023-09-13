d99db5801bc8d6da747bf9188c42da43094e2bc5
Adrien <a.bonnand@pacman.fr>
2023-09-13 11:21:14 +0200

    passage consigne 1 -> consigne 2 : création fonction choose_color et ajout de variables pour changer le nombre d'essais et la taille du code

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ def recognize_color(color):
     elif (color == "W"):
         return "White"
 
+def choose_color():
+    color=["None"]*6
+    initial=["None"]*6
+
+    print("Write 6 color with her initial")
+    for idx in range(6):
+        color[idx]=input("Write color "+str(idx+1))
+        initial[idx]=input("Write initial")
+
+    return [color,initial]
 
 '''Ask the user to obtain a potential true combinaison'''
 def ask_combinaison():
@@ -27,12 +37,11 @@ def ask_combinaison():
 
     return(combinaison)
 
-def random_combinaison():
-    random_list=["None"]*4
-    random_colors_list=["R","G","B","Y","P","W"]
+def random_combinaison(color_choosed,code_lenght):
+    random_list=["None"]*code_lenght
 
-    for idx in range(4):
-        random_list[idx]=random.choice(random_colors_list)
+    for idx in range(code_lenght):
+        random_list[idx]=random.choice(color_choosed)
 
     return random_list
 
@@ -40,17 +49,20 @@ def random_combinaison():
 def number_partial_colors(user_combinaison,true_combinaison,number_correct):
 
     number=0
+    list=user_combinaison
+
+    for idx in range(len(user_combinaison)):
+        for idx2 in range(len(user_combinaison)):
+            if(user_combinaison[idx]==true_combinaison[idx2]):
+                list[idx]="_"
 
     for first_idx in range(len(user_combinaison)):
         for second_idx in range(len(true_combinaison)):
-            if(user_combinaison[first_idx]==true_combinaison[second_idx] and '''
-            
-                    user_combinaison[second_idx]==true_combinaison[second_idx]'''):
+
+            if(user_combinaison[first_idx]==true_combinaison[second_idx]):
                 number+=1
 
-    number_partial=(number-number_correct)
-
-    return number_partial
+    return number
 
 '''test the number of correct colors in user's combinaison'''
 def number_correct_colors(user_combinaison,true_combinaison):
@@ -67,11 +79,17 @@ def number_correct_colors(user_combinaison,true_combinaison):
 
 def game():
     try_number=0
+    try_max=12
+    code_lenght=4
     win=False
-    true_combinaison = random_combinaison()
+
+    choose_color()
+    print("code lenght : "+ str(code_lenght))
+    #true_combinaison = random_combinaison(choose_color[1],code_lenght)
+    true_combinaison=["G","R","B","Y"]
     print(true_combinaison)
 
-    while(try_number!=12 and win==False):
+    while(try_number!=try_max and win==False):
 
         user_combinaison = ask_combinaison()
         number_of_correct_colors=number_correct_colors(user_combinaison,true_combinaison)
@@ -92,4 +110,4 @@ def game():
     else:
         print("You lose sorry :( ")
 
-game()
+
